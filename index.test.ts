e2eea9df8c96a6cdcdde44660b8c0b621c6073a6
Rocktim Saikia <rocktimthedev@gmail.com>
2023-04-05 01:10:15 +0530

    Refactor number formatting and add build watch script in package.json and index.ts

--- index.test.ts
+++ index.test.ts
@@ -2,18 +2,18 @@ import test from 'ava';
 import prettyNums from './dist/index.js';
 
 test('Should format numbers correctly', (t) => {
-	t.is(prettyNums(42), '42');
-	t.is(prettyNums(1234), '1.2K');
-	t.is(prettyNums(5000), '5.0K');
-	t.is(prettyNums(2500000), '2.5M');
-	t.is(prettyNums(123456789), '123.5M');
-	t.is(prettyNums(9876543210), '9.9B');
-	t.is(prettyNums(1234567890123), '1.2T');
+  t.is(prettyNums(42), '42');
+  t.is(prettyNums(1234), '1.2K');
+  t.is(prettyNums(5000), '5.0K');
+  t.is(prettyNums(2500000), '2.5M');
+  t.is(prettyNums(123456789), '123.5M');
+  t.is(prettyNums(9876543210), '9.9B');
+  t.is(prettyNums(1234567890123), '1.2T');
 });
 
 test('Should handle negative numbers', (t) => {
-	t.is(prettyNums(-42), '-42');
-	t.is(prettyNums(-1234), '-1.2K');
-	t.is(prettyNums(-2500000), '-2.5M');
-	t.is(prettyNums(-9876543210), '-9.9B');
+  t.is(prettyNums(-42), '-42');
+  t.is(prettyNums(-1234), '-1.2K');
+  t.is(prettyNums(-2500000), '-2.5M');
+  t.is(prettyNums(-9876543210), '-9.9B');
 });
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 function formatNumber(num: number, divisor: number, suffix: string): string {
-	const sign = num < 0 ? '-' : '';
-	const absNum = Math.abs(num);
-	return sign + (absNum / divisor).toFixed(1) + suffix;
+  const sign = num < 0 ? '-' : '';
+  const absNum = Math.abs(num);
+  return sign + (absNum / divisor).toFixed(1) + suffix;
 }
 
 export default function prettyNums(num: number): string {
-	const absNum = Math.abs(num);
-	if (absNum < 1e3) return num.toString();
-	if (absNum >= 1e3 && absNum < 1e6) return formatNumber(num, 1e3, 'K');
-	if (absNum >= 1e6 && absNum < 1e9) return formatNumber(num, 1e6, 'M');
-	if (absNum >= 1e9 && absNum < 1e12) return formatNumber(num, 1e9, 'B');
-	return formatNumber(num, 1e12, 's');
+  const absNum = Math.abs(num);
+  if (absNum < 1e3) return num.toString();
+  if (absNum >= 1e3 && absNum < 1e6) return formatNumber(num, 1e3, 'K');
+  if (absNum >= 1e6 && absNum < 1e9) return formatNumber(num, 1e6, 'M');
+  if (absNum >= 1e9 && absNum < 1e12) return formatNumber(num, 1e9, 'B');
+  return formatNumber(num, 1e12, 'T');
 }
